c85a17dbb100c539dfa9e881e2970338f3af6ef4
Gabriel Ferreira <diangellis4@gmail.com>
2026-09-13 13:27:07 -0300

    feat(models): mapeia cursos, disciplinas e matriz curricular

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.core.database import Base
+from app.models.curso import Curso, CursoDisciplina
+from app.models.disciplina import Disciplina
+
+__all__ = [
+    "Base",
+    "Curso",
+    "CursoDisciplina",
+    "Disciplina"
+]
